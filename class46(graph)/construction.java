b7e34cbbcaada0547893d7e4fb8b3aec77334466
lovearya <lovepreetarya1405@gmail.com>
2022-02-23 17:31:43 +0530

    class 46 work

--- class46(graph)/construction.java
+++ class46(graph)/construction.java
@@ -20,9 +20,9 @@ public class construction {
         int[][] edges={
             {0,1,6},
             {0,3,5},
-            {1,2,2},
+            // {1,2,2},
             {3,2,0},
-            {2,4,3},
+            // {2,4,3},
             {4,5,6},
             {4,6,2},
             {5,6,3}
@@ -31,13 +31,122 @@ public class construction {
         int vertices = 7;
         make(edges,vertices);
 
+        ques();
+
+
+        
+    }
+
+    static void ques(){
         // displayGraph();
         
-        System.out.println("---------------------------");
-        removeEdge(1,2);
+        // System.out.println("---------------------------");
         
-        displayGraph();
+        // removeEdge(1,2);
         
+        // removeVertex(2);
+        // displayGraph();
+
+        boolean[] visited = new boolean[graph.size()];
+        // dfs(0,visited,"0==>");
+        // dfsSvToDv(0,5,visited,"0==>");
+
+        // System.out.println(findCycle(0, -1, visited));
+
+        //cycle in disconnected Graph
+
+        // for(ArrayList ed:graph){
+            //graph.get i all nodes ke liye call lagayege
+            // ager boolen wale me not visited hai to 
+        // }
+
+        boolean[] visit = new boolean[graph.size()];
+        cycleInDisconnected(visit, visited);
+
+    }
+
+    static void cycleInDisconnected(boolean[] visit,boolean[] visited){
+        for(int i=0;i<graph.size();i++){
+            if(!visit[i]){
+                System.out.println(findCycle(i, -1 ,visit, visited));
+            }
+        }
+    }
+
+    static boolean findCycle(int cv,int pv,boolean[] visit, boolean[] visited){
+
+        visited[cv]=true;
+        visit[cv] = true;
+
+        boolean flag=false;
+
+        for(edge ed:graph.get(cv)){
+
+            if(visited[ed.v] && ed.v!=pv){
+                return true;
+            }
+
+            if(!visited[ed.v]){
+                // System.out.println(flag=flag);
+                flag=flag || findCycle(ed.v,cv,visit,visited);
+            }
+        }
+
+        visited[cv]=false;
+
+        return flag;
+
+    }
+
+    static void dfs(int cv,boolean[] visited,String psf){
+        visited[cv] = true;
+
+        boolean flag=false;
+
+        for(edge ed:graph.get(cv)){
+            if(!visited[ed.v]){
+                flag=true;
+                dfs(ed.v,visited,psf+ed.v+"==>");
+            }
+        }
+
+        if(!flag){
+            System.out.println(psf);
+        }
+
+        visited[cv] = false;
+
+    }
+    static void dfsSvToDv(int cv,int dv,boolean[] visited,String psf){
+        visited[cv] = true;
+
+
+        for(edge ed:graph.get(cv)){
+
+            if(ed.v==dv){
+                System.out.println(psf+dv);
+            }else  if(!visited[ed.v]){
+                dfsSvToDv(ed.v,dv,visited,psf+ed.v+"==>");
+            }
+        }
+
+        visited[cv] = false;
+
+    }
+
+    static void removeVertex(int ver){
+        ArrayList<edge> edges=graph.get(ver);
+        for(edge ed:edges){
+            ArrayList<edge> rmed = graph.get(ed.v);
+            for(int i=0;i<rmed.size();i++){
+                if(ver==rmed.get(i).v){
+                    rmed.remove(i);
+                    break;
+                }
+            }
+        }
+
+        graph.set(ver, new ArrayList<edge>());
     }
 
     static void removeEdge(int u,int v){
